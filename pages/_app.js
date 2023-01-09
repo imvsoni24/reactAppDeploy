240a9a3c9ec996fe73cd876687748c941c991ffb
imvsoni24 <imvsoni24@gmail.com>
2023-01-09 09:53:17 +0530

    hello

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
 import Navbar from '../component/Navbar'
 import '../styles/globals.css'
+import {ChakraProvider} from "@chakra-ui/react"
 
 export default function App({ Component, pageProps }) {
-  return  (<>
-  <Navbar/>
-  <Component {...pageProps} />
-  </> )
+  return  (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
