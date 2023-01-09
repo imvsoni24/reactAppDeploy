c3d3b0e3fe03206cc24ee599c7b280b70c1607ef
imvsoni24 <imvsoni24@gmail.com>
2023-01-09 09:22:35 +0530

    hello

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
+import Navbar from '../component/Navbar'
 import '../styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return  (<>
+  <Navbar/>
+  <Component {...pageProps} />
+  </> )
 }
